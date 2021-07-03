b3f3c5a7f524c5bf7ffd25ad38f86ed69490cd87
alestor123 <alestoraldous@gmail.com>
2021-07-03 18:42:36 +0530

    added test

---
+++ test.js
@@ -0,0 +1,13 @@
+const tap = require('tap')
+const cp = require('./App')
+tap.test('Error test', async t => {
+await t.rejects(cp(),{ message: 'Please enter a vaild package name' })
+await t.rejects(cp(''),{ message: 'Please enter a vaild package name' })
+await t.rejects(cp(1),{ message: 'Please enter a vaild package name' })
+
+})
+tap.test('Output test', async t => {
+    const main = await cp('meow')
+    t.equal(typeof main,'object')
+    t.equal(Math.round(main.map(mn => mn.depsPercentage).reduce((a, b) => a + b, 0)),100)
+})
